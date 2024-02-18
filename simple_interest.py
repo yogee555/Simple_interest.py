20d8fb9cff7d1488360441a6c46008e4d199ded2
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2024-02-18 18:08:20 +0530

    Add files via upload

---
+++ simple_interest.py
@@ -0,0 +1,6 @@
+# calculate simple interest
+Principle = float(input("Enter the principle amount"))
+Rate = float(input("Enter rate"))
+Time = float(input("Enter the time(in year)"))
+Simple_interest = Principle + Rate + Time
+print("The simple interest is",Simple_interest)
